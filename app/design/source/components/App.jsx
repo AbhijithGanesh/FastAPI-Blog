9b9228f9ad14d70e8380e4ec1d68e4116d448072
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-08 11:26:34 +0530

    Initial config for React components

---
+++ app/design/source/components/App.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Design = () => {
+    return (
+        <h1>
+            Hello World
+        </h1>
+    )
+}
+export default Design;
---
+++ app/design/source/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Design from "./components/App.jsx"
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Design/>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
