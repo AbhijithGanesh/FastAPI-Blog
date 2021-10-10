6cf60580048741603cc8fa12fccbad88f5a39440
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2021-10-10 22:47:47 +0530

    Delete app/API directory

--- app/API/Blogs/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-from Blogs.routes.post_router import posts
-
-app = FastAPI()
-
-app.include_router(posts, prefix="/posts")
-
-
--- app/API/Blogs/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class BlogTable(models.Model):
-    Title = models.CharField(max_length = 255, default = "No Title", null = False, blank = False )
-    SubTitle = models.CharField(max_length = 255, default = "No SubTitle", null = False, blank = False )
-    Date_of_creation = models.DateTimeField(auto_created=True)
-    Content = models.TextField()
-    URL = models.URLField()
