4c93bb246fb5374ac8d2db329dfa73d866e940f8
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-20 12:33:54 +0530

    delisting

--- app/BlogSite/logic/routers/posts.py
+++ app/BlogSite/logic/routers/posts.py
@@ -7,15 +7,13 @@ posts_fetcher = APIRouter()
 @posts_fetcher.get("/get-all-objects/")
 def get_all_obj():
     from Logic.models import BlogTable
-
-    objs: list = list(BlogTable.objects.all())
+    objs: list = BlogTable.objects.all()
     return objs
 
 
 @posts_fetcher.get("/{post_id}")
 def query_posts(post_id: str):
     from Logic.models import BlogTable
-
     query = BlogTable.objects.get(Identifier=post_id)
     return [query]
 
