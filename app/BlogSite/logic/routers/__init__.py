16161a4e7e42beb3f72b7c5c0e2911828fb18ca7
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-20 21:57:26 +0530

    New user routes imported to init

--- app/BlogSite/logic/routers/__init__.py
+++ app/BlogSite/logic/routers/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ("get_posts", "template_router")
+__all__ = ("get_posts", "template_router", "user_router")
 
 from .templates import templates_view as template_router
 from .posts import posts_fetcher as get_posts
+from .user_routes import user_router as user_router
