7a11b1d3f238349952de83e64e9dea6a4c338d0f
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-10-26 22:02:08 +0530

    Addition of POST method to route

--- app/BlogSite/logic/routers/posts.py
+++ app/BlogSite/logic/routers/posts.py
@@ -1,8 +1,19 @@
+from typing import Text
 from fastapi import APIRouter
-
+from pydantic import BaseModel, AnyHttpUrl
+import uuid
+from datetime import date
 
 posts_fetcher = APIRouter()
 
+class Blog(BaseModel):
+    id: int
+    Title: str
+    Identifier: Text
+    SubTitle: str
+    # Date_of_creation: date
+    Content: str
+    URL: AnyHttpUrl
 
 @posts_fetcher.get("/get-all-objects/")
 def get_all_obj():
@@ -15,11 +26,12 @@ def get_all_obj():
 @posts_fetcher.get("/{post_id}")
 def query_posts(post_id: str):
     from Logic.models import BlogTable
-
-    query = BlogTable.objects.get(Identifier=post_id)
+    query = BlogTable.objects.get(id=post_id)
     return [query]
 
 
 @posts_fetcher.post("/post/{Title}")
-def post_obj(Title: str):
-    ...
+def post_obj(Title: str, item: Blog):
+    from Logic.models import BlogTable
+    val = BlogTable(**dict(item))
+    return val
